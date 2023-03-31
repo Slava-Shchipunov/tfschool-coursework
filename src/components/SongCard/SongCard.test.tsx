d81efbdbe6c749adc5d719b74bd29079cabb4ff7
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-01 04:09:13 +0700

    test: add tests for the SongCard component

---
+++ src/components/SongCard/SongCard.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { SongCard } from './SongCard';
+
+const props = {
+  imgUrl: 'https://i.scdn.co/image/ab67616d0000b273ec182939cba3386ddcb93069',
+  artist: 'IMAGINE DRAGON',
+  title: 'Wrecked',
+  isSmall: true,
+};
+
+test('renders SongCard component', () => {
+  render(
+    <SongCard
+      imgUrl={props.imgUrl}
+      artist={props.artist}
+      title={props.title}
+      isSmall={props.isSmall}
+    />
+  );
+  const title = screen.getByText(props.title);
+  const artist = screen.getByText(props.artist);
+  expect(title).toBeInTheDocument();
+  expect(artist).toBeInTheDocument();
+});
