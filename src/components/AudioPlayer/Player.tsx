7968bad5cc3ececd09cd2a8930ac31c0ea5e4c06
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-04 14:37:01 +0700

    feat: add Player component

---
+++ src/components/AudioPlayer/Player.tsx
@@ -0,0 +1,24 @@
+import { useRef } from 'react';
+
+/* eslint-disable jsx-a11y/media-has-caption */
+type TPlayer = {
+  src: string;
+  isPlay: boolean;
+};
+
+export function Player(props: TPlayer) {
+  const { src, isPlay } = props;
+
+  const audioRef = useRef(null);
+
+  if (audioRef.current) {
+    const curr: HTMLAudioElement = audioRef.current;
+    if (isPlay) {
+      curr.play();
+    } else {
+      curr.pause();
+    }
+  }
+
+  return <audio src={src} ref={audioRef} data-testid="audio" />;
+}
