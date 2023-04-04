ce4fce62206f0450a7705105fb545f84fdb60065
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-04 14:41:36 +0700

    test: update tests for the WelcomePage

--- src/pages/WelcomePage.test.tsx
+++ src/pages/WelcomePage.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { WelcomePage } from 'pages/WelcomePage';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
 
 test('renders WelcomePage', () => {
   render(
-    <MemoryRouter>
-      <WelcomePage />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter>
+        <WelcomePage />
+      </MemoryRouter>
+    </Provider>
   );
   const WelcomePageElement = screen.getByText(/You are on the WelcomePage/i);
   expect(WelcomePageElement).toBeInTheDocument();
