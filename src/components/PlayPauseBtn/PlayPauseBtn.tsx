7d44e3f0d141c2b96c98478276a8cc37414582b8
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-04 14:36:15 +0700

    feat: add playPauseBtn component

--- src/components/PlayPauseBtn/PlayPauseBtn.tsx
+++ src/components/PlayPauseBtn/PlayPauseBtn.tsx
@@ -1,21 +1,23 @@
+import classNames from 'classnames/bind';
 import styles from './style.module.css';
 
 type TPlayPauseBtn = {
   isPlay: boolean;
+  playPauseTrack: () => void;
 };
 
+const className = classNames.bind(styles);
+
 export function PlayPauseBtn(props: TPlayPauseBtn) {
-  const { isPlay } = props;
+  const { isPlay, playPauseTrack } = props;
 
   return (
     <button
-      className={
-        isPlay
-          ? `${styles.playPauseBtn} ${styles.pause}`
-          : `${styles.playPauseBtn}`
-      }
+      className={className('play-pause-btn', { pause: isPlay })}
       type="button"
       aria-label="Play or pause"
+      onClick={playPauseTrack}
+      data-testid="playPauseBtn"
     />
   );
 }
