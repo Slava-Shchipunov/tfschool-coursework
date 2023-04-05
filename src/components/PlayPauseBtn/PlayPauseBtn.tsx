14b48597e2ff47133c60a19009851ea148519cef
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-06 01:21:35 +0700

    feat: add functionality from play-pause-btn branch

---
+++ src/components/PlayPauseBtn/PlayPauseBtn.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames/bind';
+import styles from './style.module.css';
+
+type TPlayPauseBtn = {
+  isPlay: boolean;
+  playPauseTrack: () => void;
+};
+
+const className = classNames.bind(styles);
+
+export const PlayPauseBtn = (props: TPlayPauseBtn) => {
+  const { isPlay, playPauseTrack } = props;
+
+  return (
+    <button
+      className={className('play-pause-btn', { pause: isPlay })}
+      type="button"
+      aria-label="Play or pause"
+      onClick={playPauseTrack}
+      data-testid="playPauseBtn"
+    />
+  );
+};
