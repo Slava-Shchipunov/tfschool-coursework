36a80d71390025b190eb5d6a3d91c7e30cbb7dd1
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-07 22:09:19 +0700

    refactor: move the selector from AudioPlayer.tsx
    Move the selector from AudioPlayer to a separate file

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -2,16 +2,14 @@ import classNames from 'classnames/bind';
 import { useDispatch, useSelector } from 'react-redux';
 import { PlayPauseBtn } from 'components/AudioPlayer/PlayPauseBtn/PlayPauseBtn';
 import { togglePlay } from 'store/player/player.slice';
-import { TRootState } from 'store/store';
 import styles from './style.module.css';
 import { Player } from './Player';
+import { getPlayer } from './selectors/getPlayer';
 
 const className = classNames.bind(styles);
 
 export const AudioPlayer = () => {
-  const { activeSong, isPlay } = useSelector(
-    (state: TRootState) => state.player
-  );
+  const { activeSong, isPlay } = useSelector(getPlayer);
   const dispatch = useDispatch();
 
   const playPauseTrack = () => {
