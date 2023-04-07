846a7e85df2f3ea2e07cd33981ee0088060defdc
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-07 23:13:12 +0700

    refactor: move inputs elements in forms

--- src/components/Forms/SignInForm.tsx
+++ src/components/Forms/SignInForm.tsx
@@ -26,42 +26,38 @@ export const SignInForm = () => {
 
   return (
     <form className={className('form')} onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="login" className={className('lable')}>
-        Login <br />
-        <input
-          className={className('input')}
-          id="login"
-          type="text"
-          placeholder="Login"
-          autoComplete="off"
-          {...register('login', {
-            required: 'Login is required',
-            minLength: {
-              value: 4,
-              message: 'Login should be at-least 4 characters',
-            },
-          })}
-        />
-        <p className={className('error-msg')}>{errors.login?.message}</p>
-      </label>
-      <label htmlFor="password" className={className('lable')}>
-        Password <br />
-        <input
-          className={className('input')}
-          id="password"
-          type="password"
-          placeholder="Password"
-          autoComplete="off"
-          {...register('password', {
-            required: 'Password is required',
-            minLength: {
-              value: 5,
-              message: 'Password should be at-least 5 characters',
-            },
-          })}
-        />
-        <p className={className('error-msg')}>{errors.password?.message}</p>
-      </label>
+      <label htmlFor="login">Login</label>
+      <input
+        className={className('input')}
+        id="login"
+        type="text"
+        placeholder="Login"
+        autoComplete="off"
+        {...register('login', {
+          required: 'Login is required',
+          minLength: {
+            value: 4,
+            message: 'Login should be at-least 4 characters',
+          },
+        })}
+      />
+      <p className={className('error-msg')}>{errors.login?.message}</p>
+      <label htmlFor="password">Password</label>
+      <input
+        className={className('input')}
+        id="password"
+        type="password"
+        placeholder="Password"
+        autoComplete="off"
+        {...register('password', {
+          required: 'Password is required',
+          minLength: {
+            value: 5,
+            message: 'Password should be at-least 5 characters',
+          },
+        })}
+      />
+      <p className={className('error-msg')}>{errors.password?.message}</p>
       <button className={className('button')} type="submit">
         Sign in
       </button>
--- src/components/Forms/SignUpForm.tsx
+++ src/components/Forms/SignUpForm.tsx
@@ -28,59 +28,59 @@ export const SignUpForm = () => {
   return (
     <form className={className('form')} onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="login" className={className('lable')}>
-        Login <br />
-        <input
-          className={className('input')}
-          id="login"
-          type="text"
-          placeholder="Login"
-          autoComplete="off"
-          {...register('login', {
-            required: 'Login is required',
-            minLength: {
-              value: 4,
-              message: 'Login should be at-least 4 characters',
-            },
-          })}
-        />
-        <p className={className('error-msg')}>{errors.login?.message}</p>
+        Login
       </label>
+      <input
+        className={className('input')}
+        id="login"
+        type="text"
+        placeholder="Login"
+        autoComplete="off"
+        {...register('login', {
+          required: 'Login is required',
+          minLength: {
+            value: 4,
+            message: 'Login should be at-least 4 characters',
+          },
+        })}
+      />
+      <p className={className('error-msg')}>{errors.login?.message}</p>
       <label htmlFor="email" className={className('lable')}>
-        Email <br />
-        <input
-          className={className('input')}
-          id="email"
-          type="text"
-          placeholder="Email"
-          autoComplete="off"
-          {...register('email', {
-            required: 'Email is required',
-            pattern: {
-              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-              message: 'Email is not valid',
-            },
-          })}
-        />
-        <p className={className('error-msg')}>{errors.email?.message}</p>
+        Email
       </label>
+      <input
+        className={className('input')}
+        id="email"
+        type="text"
+        placeholder="Email"
+        autoComplete="off"
+        {...register('email', {
+          required: 'Email is required',
+          pattern: {
+            value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+            message: 'Email is not valid',
+          },
+        })}
+      />
+      <p className={className('error-msg')}>{errors.email?.message}</p>
       <label htmlFor="password" className={className('lable')}>
-        Password <br />
-        <input
-          className={className('input')}
-          id="password"
-          type="password"
-          placeholder="Password"
-          autoComplete="off"
-          {...register('password', {
-            required: 'Password is required',
-            minLength: {
-              value: 5,
-              message: 'Password should be at-least 5 characters',
-            },
-          })}
-        />
-        <p className={className('error-msg')}>{errors.password?.message}</p>
+        Password
       </label>
+      <input
+        className={className('input')}
+        id="password"
+        type="password"
+        placeholder="Password"
+        autoComplete="off"
+        {...register('password', {
+          required: 'Password is required',
+          minLength: {
+            value: 5,
+            message: 'Password should be at-least 5 characters',
+          },
+        })}
+      />
+      <p className={className('error-msg')}>{errors.password?.message}</p>
       <button className={className('button')} type="submit">
         Sign up
       </button>
