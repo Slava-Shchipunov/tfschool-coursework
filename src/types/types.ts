aa05e47b5505c255e11ba824da9c8c58e18741a2
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-22 03:19:37 +0700

    feat: add spotify api response types

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,3 +9,35 @@ export type TUser = {
 export type TUserCreate = Omit<TUser, 'id'>;
 
 export type TUserAuth = Omit<TUser, 'id' | 'login'>;
+
+export type TTrack = {
+  id: string;
+  name: string;
+  artists: string[];
+  image: string;
+};
+
+export type TTrackResponse = {
+  data: {
+    id: string;
+    name: string;
+    albumOfTrack: {
+      coverArt: {
+        sources: TImageResponse[];
+      };
+    };
+    artists: { items: TArtistResponse[] };
+  };
+};
+
+export type TArtistResponse = { profile: { name: string } };
+
+export type TImageResponse = {
+  height: number;
+  url: string;
+  width: number;
+};
+
+export type TSearchResponse = {
+  tracks: TTrackResponse[];
+};
