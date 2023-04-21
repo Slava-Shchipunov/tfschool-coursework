689df5e4b3d0c241ad9bbd3350d086dcba441018
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-22 03:18:20 +0700

    feat: add searchTracks and getTrackDetails request
    Also add instanceAxiosSpotify config

---
+++ src/api/tracks.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { RAPIDAPI_KEY, SPOTIFY_BASE_URL } from 'consts';
+import { TSearchResponse } from 'types/types';
+
+const instanceAxiosSpotify = (defaultConfig?: AxiosRequestConfig) => {
+  const config: AxiosRequestConfig = {
+    baseURL: `https://${SPOTIFY_BASE_URL}`,
+    headers: {
+      'X-RapidAPI-Key': RAPIDAPI_KEY,
+      'X-RapidAPI-Host': SPOTIFY_BASE_URL,
+    },
+  };
+
+  return axios.create({
+    ...config,
+    ...defaultConfig,
+  });
+};
+
+export const searchTracks = (searchQuery: string) => {
+  return instanceAxiosSpotify().get<TSearchResponse>('/search', {
+    params: {
+      q: searchQuery,
+      type: 'tracks',
+      offset: '0',
+      limit: '20',
+      numberOfTopResults: '5',
+    },
+  });
+};
+
+export const getTrackDetails = (tarckId: string) => {
+  return instanceAxiosSpotify().get('/tracks', {
+    params: { ids: tarckId },
+  });
+};
--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,7 @@ export const FIREBASE_CONFIG = {
   messagingSenderId: '397367957174',
   appId: '1:397367957174:web:de6d328803db85d8f5b817',
 };
+
+export const RAPIDAPI_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
+
+export const SPOTIFY_BASE_URL = 'spotify81.p.rapidapi.com';
