392853b0091bd4a1f181080cc6a14f54f29bc20f
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-04-22 03:31:52 +0700

    feat: add SearchPage

---
+++ src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames/bind';
+import styles from './style.module.css';
+import { useSelector } from 'react-redux';
+import { getTracks } from './selectors/getTracks';
+import { SongCard } from 'components/SongCard/SongCard';
+import { searchTracksThunk } from 'store/tracks/tracks.thunk';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+
+const className = classNames.bind(styles);
+
+export const SearchPage = () => {
+  const { isLoading, errorMessage, trackList } = useSelector(getTracks);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className={className('search-page')}>
+      <form
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          const target = event.target as HTMLFormElement;
+          const data: string = target.search.value;
+          dispatch(searchTracksThunk(data));
+        }}
+      >
+        <input type="text" name="search" />
+      </form>
+      <div className={className('tracks')}>
+        {trackList &&
+          trackList.map((track) => (
+            <SongCard
+              key={track.id}
+              imgUrl={track.image}
+              title={track.name}
+              artist={track.artists.join(', ')}
+              isSmall={true}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
