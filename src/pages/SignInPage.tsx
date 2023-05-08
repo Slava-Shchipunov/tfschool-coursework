e83c21e46b3f1523725b6179eacffbcca031ed2e
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-08 16:50:15 +0700

    fix: fix SignIn and SignUp pages

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,13 +1,15 @@
 import { SignInForm } from 'components/Forms/SignInForm/SignInForm';
 import { Loader } from 'components/Loader/Loader';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { getUser } from 'store/user/user.selectors';
 import classNames from 'classnames/bind';
 import styles from './style.module.css';
 import { userSignInThunk } from 'store/user/user.thunk';
 import { TUserAuth } from 'types/types';
 import { useAppDispatch } from 'hooks/useAppDispatch';
+import { auth } from 'api/firebase';
+import { Navigate } from 'react-router-dom';
+import { PathRoutes } from 'router/router';
 
 const className = classNames.bind(styles);
 
@@ -19,13 +21,13 @@ export const SignInPage = () => {
     await dispatch(userSignInThunk(data));
   };
 
+  if (auth.currentUser) {
+    return <Navigate to={`/${PathRoutes.player}`} replace />;
+  }
+
   return (
     <div className="wrapper">
-      <div className="">You are on the Sign In Page</div>
-      <Link to="/">Go to WelcomePage</Link>
-      <Link to="/sign-up">Go to Sign up page</Link>
-      <Link to="/sign-in">Go to Sign in page</Link>
-      <Link to="/ErrorPage">Go to ErrorPage</Link>
+      <h2>Sign in</h2>
       <SignInForm signIn={handleSignIn} />
       {isLoading && (
         <div className={className('loader-wrapper')}>
