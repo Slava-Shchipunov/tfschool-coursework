cd7cb05bcf3f31cb67b489f44363507e16415089
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-08 16:48:57 +0700

    fix: fix ErrorPage content

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,13 +1,11 @@
 import { Link } from 'react-router-dom';
+import { PathRoutes } from 'router/router';
 
 export const ErrorPage = () => {
   return (
     <div className="wrapper">
-      <div className="">You are on the ErrorPage</div>
-      <Link to="/">Go to WelcomePage</Link>
-      <Link to="/sign-up">Go to Sig up page</Link>
-      <Link to="/sign-in">Go to Sig in page</Link>
-      <Link to="/ErrorPage">Go to ErrorPage</Link>
+      <span>Oops, this page doesn&apos;t seem to exist.</span>
+      <Link to={PathRoutes.welcome}>Go back to main page</Link>
     </div>
   );
 };
