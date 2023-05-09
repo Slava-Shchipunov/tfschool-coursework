09c999670228abe167c6991d42b395ee03212e0a
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-09 20:38:56 +0700

    feat: add Burger btn

---
+++ src/components/Menu/Burger.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './style.module.css';
+
+const className = classNames.bind(styles);
+
+type TBurger = {
+  handleClick: () => void;
+};
+
+export const Burger = ({ handleClick }: TBurger) => {
+  return (
+    <div className={className('header-burger')} onClick={handleClick}>
+      <span className={className('burger-line')}></span>
+      <span className={className('burger-line')}></span>
+    </div>
+  );
+};
