99bbfa87256fddb7084543676c0ad306a2552446
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-14 15:57:19 +0700

    refactor: replace slice method with regex

--- src/utils/getTopTracksData.ts
+++ src/utils/getTopTracksData.ts
@@ -3,7 +3,7 @@ import { TTopTracksResponse, TTrack } from 'types/types';
 export const getTopTracksData = (tracks: TTopTracksResponse): TTrack[] => {
   return tracks.map((track) => {
     return {
-      id: track.trackMetadata.trackUri.slice(14),
+      id: track.trackMetadata.trackUri.match(/\b[\w]+$/)?.[0] ?? '',
       name: track.trackMetadata.trackName,
       artists: track.trackMetadata.artists.map((artist) => artist.name),
       image: track.trackMetadata.displayImageUri,
