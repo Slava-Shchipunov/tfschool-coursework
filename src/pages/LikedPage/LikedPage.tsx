9623ad5b1a81c85b9a5957390d13bcd2b7fccb20
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-14 16:46:44 +0700

    refactor: add SongsPageLayout component

--- src/pages/LikedPage/LikedPage.tsx
+++ src/pages/LikedPage/LikedPage.tsx
@@ -1,17 +1,13 @@
 import classNames from 'classnames/bind';
 import styles from './style.module.css';
-import { useSelector } from 'react-redux';
-import { getTracks } from 'store/tracks/tracks.selectors';
-import { SongCard } from 'components/SongCard/SongCard';
 // import { searchTracksThunk } from 'store/tracks/tracks.thunk';
 // import { useAppDispatch } from 'hooks/useAppDispatch';
-import { Loader } from 'components/Loader/Loader';
+import { SongsPageLayout } from 'pages/PlayerPage/SongsPageLayout';
 // import { useEffect } from 'react';
 
 const className = classNames.bind(styles);
 
 export const LikedPage = () => {
-  const { isLoading, errorMessage, currentSongs } = useSelector(getTracks);
   // const dispatch = useAppDispatch();
 
   // TODO получать понравившиеся песни при монтировании компонента
@@ -21,25 +17,7 @@ export const LikedPage = () => {
 
   return (
     <div className={className('liked-page')}>
-      {isLoading && <Loader />}
-      {!isLoading && (
-        <>
-          <h2>Liked Songs</h2>
-          <div className={className('tracks')}>
-            {currentSongs &&
-              currentSongs.map((track) => (
-                <SongCard
-                  key={track.id}
-                  trackId={track.id}
-                  imgUrl={track.image}
-                  title={track.name}
-                  artist={track.artists.join(', ')}
-                  isSmall={true}
-                />
-              ))}
-          </div>
-        </>
-      )}
+      <SongsPageLayout pageTitle={'Liked Songs'} />
     </div>
   );
 };
--- src/pages/TopPage/TopPage.tsx
+++ src/pages/TopPage/TopPage.tsx
@@ -1,17 +1,13 @@
 import classNames from 'classnames/bind';
 import styles from './style.module.css';
-import { useSelector } from 'react-redux';
-import { getTracks } from 'store/tracks/tracks.selectors';
-import { SongCard } from 'components/SongCard/SongCard';
 import { useAppDispatch } from 'hooks/useAppDispatch';
-import { Loader } from 'components/Loader/Loader';
 import { getTopTracksThunk } from 'store/tracks/tracks.thunk';
 import { useEffect } from 'react';
+import { SongsPageLayout } from 'pages/PlayerPage/SongsPageLayout';
 
 const className = classNames.bind(styles);
 
 export const TopPage = () => {
-  const { isLoading, errorMessage, currentSongs } = useSelector(getTracks);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -20,25 +16,7 @@ export const TopPage = () => {
 
   return (
     <div className={className('top-page')}>
-      {isLoading && <Loader />}
-      {!isLoading && (
-        <>
-          <h2>Popular Now</h2>
-          <div className={className('tracks')}>
-            {currentSongs &&
-              currentSongs.map((track) => (
-                <SongCard
-                  key={track.id}
-                  trackId={track.id}
-                  imgUrl={track.image}
-                  title={track.name}
-                  artist={track.artists.join(', ')}
-                  isSmall={true}
-                />
-              ))}
-          </div>
-        </>
-      )}
+      <SongsPageLayout pageTitle={'Popular Now'} />
     </div>
   );
 };
