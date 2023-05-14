6ef116aff901f4f92b74b2a8d232a6ff949caed3
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-14 15:17:33 +0700

    refactor: rewrite getDateOfLastThursday
    Rewrite getDateOfLastThursday with date-fns library

--- src/utils/getDateOfLastThursday.ts
+++ src/utils/getDateOfLastThursday.ts
@@ -1,13 +1,7 @@
+import { format, previousThursday } from 'date-fns';
+
 export const getDateOfLastThursday = (): string => {
-  const currentDate = new Date();
-  const msInOneDay = 86400000;
-  const lastThursday = new Date(
-    Date.now() - msInOneDay * (3 + currentDate.getUTCDay())
-  );
-  const date = [
-    lastThursday.getUTCFullYear(),
-    lastThursday.getUTCMonth() + 1,
-    lastThursday.getUTCDate(),
-  ];
-  return date.map((el) => String(el).padStart(2, '0')).join('-');
+  const previousThursdayDate = previousThursday(new Date());
+
+  return format(previousThursdayDate, 'yyyy-MM-dd');
 };
