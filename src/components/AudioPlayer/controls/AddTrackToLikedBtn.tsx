3dbf95e62d56a476f7f0aa3b489de84062ae9236
Slava-Shchipunov <shchipunovsd@gmail.com>
2023-05-17 16:00:06 +0700

    feat: memoize a components

--- src/components/AudioPlayer/controls/AddTrackToLikedBtn.tsx
+++ src/components/AudioPlayer/controls/AddTrackToLikedBtn.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import styles from '../style.module.css';
 import { Icon } from 'components/Icon/Icon';
 import likedIconUrl from 'assets/svg/heartBtn.svg';
+import { memo } from 'react';
 
 type TAddTrackToLikedBtn = {
   isActive: boolean;
@@ -11,7 +12,7 @@ type TAddTrackToLikedBtn = {
 
 const className = classNames.bind(styles);
 
-export const AddTrackToLikedBtn = (props: TAddTrackToLikedBtn) => {
+export const AddTrackToLikedBtnComponent = (props: TAddTrackToLikedBtn) => {
   const { isTrackLiked, isActive, addTrackToLiked } = props;
 
   return (
@@ -27,3 +28,5 @@ export const AddTrackToLikedBtn = (props: TAddTrackToLikedBtn) => {
     </button>
   );
 };
+
+export const AddTrackToLikedBtn = memo(AddTrackToLikedBtnComponent);
--- src/components/AudioPlayer/controls/ShuffleTracksBtn.tsx
+++ src/components/AudioPlayer/controls/ShuffleTracksBtn.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import styles from '../style.module.css';
 import { Icon } from 'components/Icon/Icon';
 import shuffleIconUrl from 'assets/svg/shuffle.svg';
+import { memo } from 'react';
 
 type TShuffleTracksBtn = {
   isShuffle: boolean;
@@ -10,7 +11,7 @@ type TShuffleTracksBtn = {
 
 const className = classNames.bind(styles);
 
-export const ShuffleTracksBtn = (props: TShuffleTracksBtn) => {
+export const ShuffleTracksBtnComponent = (props: TShuffleTracksBtn) => {
   const { isShuffle, shuffleTracks } = props;
 
   return (
@@ -25,3 +26,5 @@ export const ShuffleTracksBtn = (props: TShuffleTracksBtn) => {
     </button>
   );
 };
+
+export const ShuffleTracksBtn = memo(ShuffleTracksBtnComponent);
